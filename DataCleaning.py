822930684719feff330f735c1214b7a8f296f41c
Raymon van Dijk <raymonvandijk1997@hotmail.nl>
2018-06-18 12:21:09 +0200

    graph_example.py added doesnt yet work

--- DataCleaning.py
+++ DataCleaning.py
@@ -107,28 +107,71 @@ print(average_used)
 #         number_of_guns_stolen += int(value)
 
 # maakt dictionary met aantal guns involved
-year_dict = {}
-for i in range(len(header_dict['date'])):
-    if header_dict['date'][i][5:7] in year_dict:
-        if header_dict['n_guns_involved'][i] == 'NA':
-            year_dict[header_dict['date'][i][5:7]] += 1
-        else:
-            year_dict[header_dict['date'][i][5:7]] += int(header_dict['n_guns_involved'][i])
-    else:
-        if header_dict['n_guns_involved'][i] == 'NA':
-            year_dict[header_dict['date'][i][5:7]] = 1
-        else:
-            year_dict[header_dict['date'][i][5:7]] = int(header_dict['n_guns_involved'][i])
+# month_dict = {}
+# for i in range(len(header_dict['date'])):
+#     if header_dict['date'][i][5:7] in year_dict:
+#         if header_dict['n_guns_involved'][i] == 'NA':
+#             year_dict[header_dict['date'][i][5:7]] += 1
+#         else:
+#             year_dict[header_dict['date'][i][5:7]] += int(header_dict['n_guns_involved'][i])
+#     else:
+#         if header_dict['n_guns_involved'][i] == 'NA':
+#             year_dict[header_dict['date'][i][5:7]] = 1
+#         else:
+#             year_dict[header_dict['date'][i][5:7]] = int(header_dict['n_guns_involved'][i])
 
 
 year_dict = {}
 for i in range(len(header_dict['date'])):
-    if header_dict['date'][i][5:7] in year_dict:
-        year_dict[header_dict['date'][i][5:7]] += int(header_dict['n_killed'][i])
+    if header_dict['date'][i] in year_dict:
+        year_dict[header_dict['date'][i]] += int(header_dict['n_killed'][i])
     else:
-        year_dict[header_dict['date'][i][5:7]] = int(header_dict['n_killed'][i])
-print(year_dict)
+        year_dict[header_dict['date'][i]] = int(header_dict['n_killed'][i])
+
+import numpy as np
+
+from bokeh.plotting import figure, output_file, show
+
+dates = []
+for key in year_dict.keys():
+    dates.append(key)
+killed = np.array(year_dict.values())
+
+window_size = 30
+window = np.ones(window_size)/float(window_size)
+
+
+# output to static HTML file
+output_file("killed.html", title="normal graph")
+
+# create a new plot with a a datetime axis type
+p = figure(width=800, height=350, x_axis_type="datetime")
+
+# add renderers
+p.circle(dates, killed, size=4, color='darkgrey', alpha=0.2, legend='close')
+p.line(dates, killed, color='navy', legend='avg')
+
+# NEW: customize by setting attributes
+p.title.text = "People killed by date"
+p.legend.location = "top_left"
+p.grid.grid_line_alpha = 0
+p.xaxis.axis_label = 'Date'
+p.yaxis.axis_label = 'Killed'
+p.ygrid.band_fill_color = "olive"
+p.ygrid.band_fill_alpha = 0.1
+
+# show the results
+show(p)
+
+
+
+
+
+
 # maakt dictionary met aantal doden per state
+
+
+
 state_dict = {}
 for i in range(len(header_dict['state'])):
     if header_dict['state'][i] in state_dict:
